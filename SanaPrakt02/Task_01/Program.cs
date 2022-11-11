f61734b3054e2f98dae15973e8f1f546b0590db9
Andrew Vozniuk <ipz224_vav@student.ztu.edu.ua>
2022-11-11 17:36:32 +0200

    Changed last 2 lines

--- SanaPrakt02/Task_01/Program.cs
+++ SanaPrakt02/Task_01/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("What's up?");
 Console.WriteLine("Great");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("How was you weekend");
+Console.WriteLine("It could be much better");
